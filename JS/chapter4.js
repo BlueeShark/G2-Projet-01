41ce012b857c94921a0f3bf99621e7407449650a
BenardEmilien <emilien.benard2@gmail.com>
2024-11-28 23:07:55 +0100

    chap 4, page 1 almost done

--- JS/chapter4.js
+++ JS/chapter4.js
@@ -1,1 +1,16 @@
+document.querySelectorAll('.card').forEach(card => {
+    const button = card.querySelector('.choiceButton');
+    const imgContainer = card.querySelector('.imgContainer');
 
+    button.addEventListener('mouseenter', () => {
+        if (!imgContainer.classList.contains('flipped')) {
+            imgContainer.classList.add('flipped');
+        }
+    });
+
+    button.addEventListener('mouseleave', () => {
+        if (imgContainer.classList.contains('flipped')) {
+            imgContainer.classList.remove('flipped');
+        }
+    });
+});
