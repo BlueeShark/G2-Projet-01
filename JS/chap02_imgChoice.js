8f4b5486e309de2ec57bea165ad768d19968f851
Mattia-FR <mattia.bouvier@protonmail.com>
2024-12-03 11:14:32 +0100

    This time, the structure is really done. I hope. And the switch between light and dark mode is on.

--- JS/chap02_imgChoice.js
+++ JS/chap02_imgChoice.js
@@ -1,14 +1,35 @@
-const button = document.getElementById("javChoices");
+const button1 = document.getElementById("lightTheme");
+const button2 = document.getElementById("darkTheme");
 const body = document.body;
-const text = document.querySelectorAll(".choiceBlock p");
+const text1 = document.querySelectorAll(".choiceBlock p");
 
 function changeColorLight() {
     body.style.backgroundColor = "var(--color-three)";
-    text.forEach(function(p) {
+    button1.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.5)";
+    button2.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.5)";
+    button1.style.backgroundColor = "var(--color-three)";
+    button2.style.backgroundColor = "var(--color-three)";
+    text1.forEach(function(p) {
         p.style.backgroundColor = "var(--color-three)";
         p.style.border = "var(--color-one) solid 2px";
         p.style.color = "var(--color-one)";
+        p.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.5)";
     });
 }
 
-button.addEventListener("click", changeColorLight);
+function changeColorDark() {
+    body.style.backgroundColor = "var(--color-one)";
+    button1.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.85)";
+    button2.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.85)";
+    button1.style.backgroundColor = "var(--color-one)";
+    button2.style.backgroundColor = "var(--color-one)";
+    text1.forEach(function(p) {
+        p.style.backgroundColor = "var(--color-one)";
+        p.style.border = "var(--color-two) solid 2px";
+        p.style.color = "var(--color-two)";
+        p.style.boxShadow = "10px 4px 19px 0px rgba(0, 0, 0, 0.85)";
+    });
+}
+
+button1.addEventListener("click", changeColorLight);
+button2.addEventListener("click", changeColorDark);
