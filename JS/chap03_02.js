b577db4215e946704f4620dd4ed0724fe507b0a5
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-03 13:52:05 +0100

    add chap03 js

---
+++ JS/chap03_02.js
@@ -0,0 +1,7 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+
+btn1.onclick = () => document.body.style.backgroundColor = "grey";
+btn2.onclick = () => document.body.style.backgroundColor = "black";
+btn3.onclick = () => document.body.style.backgroundColor = "green";
